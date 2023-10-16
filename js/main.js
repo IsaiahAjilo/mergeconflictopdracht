d83cbe3257a98acc998cdd573a65429f8e10aff8
mauser051 <145978301+mauser051@users.noreply.github.com>
2023-10-16 12:02:45 +0200

    1

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
 const oldgreeting = require("./oldgreeting.js");
 console.log("merge oefening leuk!")
 
-oldgreeting.greet()
+oldgreeting()
 
 
 function basicHaiku()
 {
-    return ["Toward those short trees","We saw a batman descending","On a day in spring."]
+    return ["2","We saw a batman descending","On a day in spring."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
 
