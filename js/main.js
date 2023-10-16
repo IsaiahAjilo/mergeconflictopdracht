42b5e811ec08cc2d05367f3de5ab7f46db24b4f8
isaiahajilo <28072@ma-web.nl>
2023-10-16 11:48:05 +0200

    '1commit'

--- js/main.js
+++ js/main.js
@@ -9,7 +9,12 @@ function basicHaiku()
     return ["Toward those short trees","We saw a batman descending","On a day in spring."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
-
+function HaikuX(){
+    console.log("haiku by:","isaiah");
+    console.log("aap");
+    console.log("kaas");
+    console.log("banaan");
+}
 haikus = [
     basicHaiku()
 ]
